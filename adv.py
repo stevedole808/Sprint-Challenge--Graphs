144ed5aa99f9adbe26b136ceef3cb9973e794e0a
stevedole808 <stevedolelambda@gmail.com>
2020-09-11 07:44:32 -1000

    actually a bft, making progres

--- adv.py
+++ adv.py
@@ -28,6 +28,15 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+def get_direction(direction):
+    if direction == 'n':
+        return 's'
+    if direction == 's':
+        return 'n'
+    if direction == 'e':
+        return 'w'
+    if direction == 'w':
+        return 'e'
 
 
 def dft():
@@ -38,6 +47,47 @@ def dft():
     # for loop that initializes each rooms direction with ?
     for i in range(len(room_graph)):
         path[i] = {'n': '?', 's': '?', 'w': '?', 'e': '?'}
+    
+    # while loop that'll run only if the length of the visited set is less than the length of the room graph
+    while len(visited) < len(room_graph):
+        # set the starting/current room id to cur_room
+        cur_room = player.current_room.id
+        # set the current room path/direction to room_path
+        room_path = player.current_room.get_exits()
+        # checl to see if cur_room is in visted, if not add it to the visited set
+        if cur_room not in visited:
+            visited.add(cur_room)
+        for direction in path[cur_room]:
+            if direction not in room_path:
+                path[cur_room][direction] = 0
+
+        num_directions = 0
+        for direction in path[cur_room]:
+            if path[cur_room][direction] != '?':
+                num_directions += 1
+
+            elif not 0:
+                 move_rooms = direction
+
+        if num_directions == 4:
+            success = True
+
+        if success is True:
+            last_move = s.pop()
+            move_rooms = get_direction(last_move)
+            traversal_path.append(move_rooms)
+            player.travel(move_rooms)
+
+        else:
+            s.push(move_rooms)
+            traversal_path.append(move_rooms)
+            player.travel(move_rooms)
+            next_room = player.current_room.id
+            path[cur_room][move_rooms] = next_room
+            change_directions = get_direction(move_rooms)
+            path[next_room][change_directions] = cur_room
+            visited.add(next_room)
+
 
     
 
